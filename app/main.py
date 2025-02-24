402397e7bbe50aa0b58a903c57cc5b68c5cfcbb1
Hemant dev <54200516+INTRUDER1@users.noreply.github.com>
2025-02-24 13:45:47 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("calculator.html")  # Render the calculator UI
+    return "Hello, Jenkins Pipeline Automation!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
